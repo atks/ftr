d9fc599e0a97e182870d1d30a65b2a0759b45b89
atks <atks@umich.edu>
2022-07-28 21:17:38 +0800

    added files

---
+++ src/hmm.rs
@@ -0,0 +1,23 @@
+extern crate clap;
+use clap::{App, SubCommand, ArgMatches};
+extern crate needletail;
+use needletail::{parse_fastx_file};
+
+//make sub command interface
+pub fn make_subcmd() -> App<'static> {
+    SubCommand::with_name("len")
+
+}
+
+//core program
+pub fn run(args: &ArgMatches) {
+
+    println!("running len")
+    // let filename = &args[2];
+    // let mut reader = parse_fastx_file(&filename).expect("invalid record");
+
+    // while let Some(record) = reader.next() {
+    //     let seqrec = record.expect("invalid record");
+    //     println!("{}", seqrec.num_bases());
+    // }
+}
